15d4e65ffd3e1b13a9b99447073b416499394f87
wangrl2016 <wangrl2016@gmail.com>
2023-06-17 21:40:20 +0800

    Note class

--- ui/model/note.cc
+++ ui/model/note.cc
@@ -2,8 +2,33 @@
 // Created by wangrl2016 on 2023/6/10.
 //
 
+#include <QtGlobal>
 #include "ui/model/note.h"
 
 namespace ui {
+    Note::Note(int pos, int length, int key, float volume, float panning) :
+            selected_(false),
+            old_key_(qBound(0, key, kNumKeys)),
+            old_pos_(pos),
+            old_length_(length),
+            is_playing_(false),
+            key_(qBound(0, key, kNumKeys)),
+            volume_(volume),
+            panning_(panning),
+            length_(length),
+            pos_(pos) {}
 
+    Note::Note(const Note& note) :
+            selected_(note.selected_),
+            old_key_(note.old_key_),
+            old_pos_(note.old_pos_),
+            old_length_(note.old_length_),
+            is_playing_(note.is_playing_),
+            key_(note.key_),
+            volume_(note.volume_),
+            panning_(note.panning_),
+            length_(note.length_),
+            pos_(note.pos_) {}
+
+    Note::~Note() = default;
 }
--- ui/model/note.h
+++ ui/model/note.h
@@ -8,11 +8,84 @@
 #include "core/math/constant.h"
 
 namespace ui {
-
-
     class Note {
     public:
+        Note(int pos, int length,
+             int key, float volume, float panning);
 
+        Note(const Note& note);
+
+        ~Note();
+
+        inline void set_selected(const bool selected) { selected_ = selected; }
+
+        inline void set_old_key(const int old_key) { old_key_ = old_key; }
+
+        inline void set_old_pos(const int old_pos) { old_pos_ = old_pos; };
+
+        inline void set_old_length(const int old_length) {
+            old_length_ = old_length;
+        }
+
+        inline void set_is_playing(const bool is_playing) {
+            is_playing_ = is_playing;
+        }
+
+        void set_length(const float length) { length_ = length; }
+
+        void set_pos(const int pos) { pos_ = pos; }
+
+        void set_key(const int key) { key_ = key; }
+
+        virtual void set_volume(float volume) { volume_ = volume; }
+
+        virtual void set_panning(float panning) { panning_ = panning; }
+
+        inline bool selected() const { return selected_; }
+
+        inline int old_key() const { return old_key_; }
+
+        inline int old_pos() const { return old_pos_; }
+
+        inline int old_length() const { return old_length_; }
+
+        inline bool is_playing() const { return is_playing_; }
+
+        inline int pos() const { return pos_; }
+
+        inline int length() const { return length_; }
+
+        inline int end_pos() const { return pos_ + length_; }
+
+        inline int key() const { return key_; }
+
+        inline float volume() const { return volume_; }
+
+        inline float panning() const { return panning_; }
+
+        static inline bool LessThan(const Note* lhs, const Note* rhs) {
+            // function to compare two notes
+            if (lhs->pos() < rhs->pos()) {
+                return true;
+            } else if (lhs->pos() > rhs->pos()) {
+                return false;
+            }
+            return lhs->key() > rhs->key();
+        }
+
+    private:
+        int key_;
+        float volume_;
+        float panning_;
+        int pos_;
+        int length_;
+
+        // for piano roll editing
+        bool selected_;
+        int old_key_;
+        int old_pos_;
+        int old_length_;
+        bool is_playing_;
     };
 }
 
