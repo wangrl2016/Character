f5cc96acf6edbd2d1a682c5c594ed2b85e4766f0
wangrl2016 <wangrl2016@gmail.com>
2023-07-31 20:13:13 +0800

    String concat

--- base/string/string_concat.h
+++ base/string/string_concat.h
@@ -11,8 +11,8 @@
 namespace base {
     namespace internal {
         template<typename CharT, typename StringT>
-        void StrAppendT(std::basic_string<CharT>& dest,
-                        std::initializer_list<const StringT> pieces) {
+        void StringAppendT(std::basic_string<CharT>& dest,
+                           std::initializer_list<const StringT> pieces) {
             const size_t initial_size = dest.size();
             size_t total_size = initial_size;
             for (const auto& cur: pieces) {
@@ -28,30 +28,30 @@ namespace base {
         }
 
         template<typename StringT>
-        auto StrCatT(std::initializer_list<const StringT> pieces) {
+        auto StringConcatT(std::initializer_list<const StringT> pieces) {
             std::basic_string<typename StringT::value_type> result;
-            StrAppendT(result, pieces);
+            StringAppendT(result, pieces);
             return result;
         }
     }
 
     // Initializer list forwards to the array version.
     inline std::string StringConcat(std::initializer_list<const StringPiece> pieces) {
-        return internal::StrCatT(pieces);
+        return internal::StringConcatT(pieces);
     }
 
-    inline std::u16string StrCat(std::initializer_list<const StringPiece16> pieces) {
-        return internal::StrCatT(pieces);
+    inline std::u16string StringConcat(std::initializer_list<const StringPiece16> pieces) {
+        return internal::StringConcatT(pieces);
     }
 
-    inline void StrAppend(std::string* dest,
-                          std::initializer_list<const StringPiece> pieces) {
-        internal::StrAppendT(*dest, pieces);
+    inline void StringAppend(std::string* dest,
+                             std::initializer_list<const StringPiece> pieces) {
+        internal::StringAppendT(*dest, pieces);
     }
 
-    inline void StrAppend(std::u16string* dest,
-                          std::initializer_list<const StringPiece16> pieces) {
-        internal::StrAppendT(*dest, pieces);
+    inline void StringAppend(std::u16string* dest,
+                             std::initializer_list<const StringPiece16> pieces) {
+        internal::StringAppendT(*dest, pieces);
     }
 }
 
--- base/string/string_concat_test.cc
+++ base/string/string_concat_test.cc
@@ -2,8 +2,62 @@
 // Created by wangrl2016 on 2023/7/30.
 //
 
+#include <gtest/gtest.h>
 #include "base/string/string_concat.h"
 
 namespace base {
+    TEST(StringConcat, 8Bit) {
+        EXPECT_EQ("", StringConcat({""}));
+        EXPECT_EQ("1", StringConcat({"1"}));
+        EXPECT_EQ("122", StringConcat({"1", "22"}));
+        EXPECT_EQ("122333", StringConcat({"1", "22", "333"}));
+        EXPECT_EQ("1223334444", StringConcat({"1", "22", "333", "4444"}));
+    }
 
+    TEST(StringConcat, 16Bit) {
+        std::u16string arg1 = u"1";
+        std::u16string arg2 = u"22";
+        std::u16string arg3 = u"333";
+
+        EXPECT_EQ(u"", StringConcat({std::u16string()}));
+        EXPECT_EQ(u"1", StringConcat({arg1}));
+        EXPECT_EQ(u"122", StringConcat({arg1, arg2}));
+        EXPECT_EQ(u"122333", StringConcat({arg1, arg2, arg3}));
+    }
+
+    TEST(StringAppend, 8Bit) {
+        std::string result;
+
+        result = "foo";
+        StringAppend(&result, {std::string()});
+        EXPECT_EQ("foo", result);
+
+        result = "foo";
+        StringAppend(&result, {"1"});
+        EXPECT_EQ("foo1", result);
+
+        result = "foo";
+        StringAppend(&result, {"1", "22", "333"});
+        EXPECT_EQ("foo122333", result);
+    }
+
+    TEST(StringAppend, 16Bit) {
+        std::u16string arg1 = u"1";
+        std::u16string arg2 = u"22";
+        std::u16string arg3 = u"333";
+
+        std::u16string result;
+
+        result = u"foo";
+        StringAppend(&result, {std::u16string()});
+        EXPECT_EQ(u"foo", result);
+
+        result = u"foo";
+        StringAppend(&result, {arg1});
+        EXPECT_EQ(u"foo1", result);
+
+        result = u"foo";
+        StringAppend(&result, {arg1, arg2, arg3});
+        EXPECT_EQ(u"foo122333", result);
+    }
 }
