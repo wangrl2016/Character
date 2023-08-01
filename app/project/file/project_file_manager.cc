f3833d82b111416fcf7797a1749e7637641d007c
wangrl2016 <wangrl2016@gmail.com>
2023-08-01 15:12:25 +0800

    Generate proto file

--- app/project/file/project_file_manager.cc
+++ app/project/file/project_file_manager.cc
@@ -2,8 +2,27 @@
 // Created by wangrl2016 on 2023/7/31.
 //
 
+#include <fstream>
+#include <glog/logging.h>
+#include <QFile>
 #include "app/project/file/project_file_manager.h"
 
 namespace app {
+    std::shared_ptr<ProjectModel> ProjectFileManager::Parse(const QString& file_path) {
+        if (!QFile::exists(file_path)) {
+            LOG(ERROR) << "File " << file_path.toStdString() << " is not exist";
+            return nullptr;
+        }
 
+        std::fstream input(std::string(file_path.toLocal8Bit()), std::ios::in | std::ios::binary);
+        if (!input.is_open()) {
+            LOG(ERROR) << "Open file " << file_path.toStdString() << " failed";
+            return nullptr;
+        }
+    }
+
+    bool ProjectFileManager::Write(const std::shared_ptr<ProjectModel>& model,
+                                   const QString& file_path) {
+
+    }
 }
--- app/project/file/project_file_manager.h
+++ app/project/file/project_file_manager.h
@@ -5,8 +5,14 @@
 #ifndef CHARACTER_PROJECT_FILE_MANAGER_H
 #define CHARACTER_PROJECT_FILE_MANAGER_H
 
-namespace app {
+#include "app/model/project_model.h"
 
+namespace app {
+    class ProjectFileManager {
+        std::shared_ptr<ProjectModel> Parse(const QString& file_path);
+
+        bool Write(const std::shared_ptr<ProjectModel>& model, const QString& file_path);
+    };
 }
 
 #endif //CHARACTER_PROJECT_FILE_MANAGER_H
