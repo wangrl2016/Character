87af0cb77eddfd3405a713b17a6fcfd545c675e7
wangrl2016 <wangrl2016@gmail.com>
2023-08-02 14:51:38 +0800

    Video decoder config

--- media/base/video_decoder_config.h
+++ media/base/video_decoder_config.h
@@ -5,10 +5,31 @@
 #ifndef CHARACTER_VIDEO_DECODER_CONFIG_H
 #define CHARACTER_VIDEO_DECODER_CONFIG_H
 
-
-class video_decoder_config {
-
+extern "C" {
+#include <libavcodec/avcodec.h>
 };
+
+#include "media/base/video_aspect_ratio.h"
+
+namespace media {
+    // Describes the content of the video stream, as described by the media container
+    // (or otherwise determined by the demuxer).
+    class VideoDecoderConfig {
+    public:
+        enum class AlphaMode { kHasAlpha, kIsOpaque };
+
+        // Constructs an uninitialized object. Clients should call Initialize() with
+        // appropriate values before using.
+        VideoDecoderConfig() = default;
+
+    private:
+        AVCodecID video_codec_ = AV_CODEC_ID_NONE;
+
+        AlphaMode alpha_mode_ = AlphaMode::kIsOpaque;
+
+        VideoAspectRatio aspect_ratio_;
+    };
+}
 
 
 #endif //CHARACTER_VIDEO_DECODER_CONFIG_H
