bc3f7bdd12fc2a3a7ffbf4270d78f4b35329eeaa
wangrl2016 <wangrl2016@gmail.com>
2023-08-13 20:31:26 +0800

    Video frame layout test

--- media/base/video_frame_layout_test.cc
+++ media/base/video_frame_layout_test.cc
@@ -31,5 +31,55 @@ namespace media {
        auto num_of_planes = VideoFrame::NumPlanes(kPixelFormatI420);
         EXPECT_EQ(layout->format(), kPixelFormatI420);
         EXPECT_EQ(layout->coded_size(), coded_size);
+        EXPECT_EQ(layout->num_planes(), num_of_planes);
+        for (size_t i = 0; i < num_of_planes; i++) {
+            EXPECT_EQ(layout->planes()[i].stride, 0);
+            EXPECT_EQ(layout->planes()[i].offset, 0u);
+            EXPECT_EQ(layout->planes()[i].size, 0u);
+        }
+   }
+
+   TEST(VideoFrameLayout, CreateWithStrides) {
+       core::Size coded_size = core::Size(320, 180);
+       std::vector<int32_t> strides = {384, 192, 192};
+       auto layout = VideoFrameLayout::CreateWithStrides(kPixelFormatI420,
+                                                         coded_size,
+                                                         strides);
+
+        ASSERT_EQ(layout->format(), kPixelFormatI420);
+        ASSERT_EQ(layout->coded_size(), coded_size);
+        ASSERT_EQ(layout->num_planes(), 3u);
+        for (size_t i = 0; i < 3; i++) {
+            EXPECT_EQ(layout->planes()[i].stride, strides[i]);
+            EXPECT_EQ(layout->planes()[i].offset, 0u);
+            EXPECT_EQ(layout->planes()[i].size, 0u);
+        }
+   }
+
+    TEST(VideoFrameLayout, CreateWithPlanes) {
+       core::Size coded_size = core::Size(320, 180);
+       std::vector<int32_t> strides = {384, 192, 192};
+       std::vector<size_t> offsets = {0, 0, 200};
+       std::vector<size_t> sizes = {200, 100, 100};
+       auto layout = VideoFrameLayout::CreateWithPlanes(
+               kPixelFormatI420,
+               coded_size,
+               CreatePlanes(strides, offsets, sizes));
+
+        EXPECT_EQ(layout->format(), kPixelFormatI420);
+        EXPECT_EQ(layout->coded_size(), coded_size);
+        EXPECT_EQ(layout->num_planes(), 3u);
+        for (size_t i = 0; i < 3; i++) {
+            EXPECT_EQ(layout->planes()[i].stride, strides[i]);
+            EXPECT_EQ(layout->planes()[i].offset, offsets[i]);
+            EXPECT_EQ(layout->planes()[i].size, sizes[i]);
+        }
+   }
+
+   TEST(VideoFrameLayout, CopyConstructor) {
+       core::Size coded_size = core::Size(320, 180);
+       std::vector<int32_t> strides = {384, 192, 192};
+       std::vector<size_t> offsets = {0, 0, 200};
+       std::vector<size_t> sizes = {200, 100, 100};
    }
 }
